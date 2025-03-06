38368fedae3a418edaa4e0e738ea8913c91262c4
Blaqkstar <111619118+Blaqkstar@users.noreply.github.com>
2025-03-06 09:40:01 -0600

    Implemented some very slightly different item handling to ref. item instead of name where relevant Implemented Observer pattern to compare relative toggled status of Conservatory levers Fixed usable item coloring of key in Tutorial Room Added some additional debug feedback for item usage info

--- src/dev/java/game/PottedPlant.java
+++ src/dev/java/game/PottedPlant.java
@@ -7,10 +7,15 @@ public class PottedPlant extends Item{
     }
     @Override
     public void use() {
-        this.setUsed(true);
-        System.out.println(ConsoleColors.RED+"ACTION"+ConsoleColors.RESET+": You move the potted plant and even remove some of the dirt, but nothing of interest here.");
-        setDescription("a plant in a pot");
-        setInspection(ConsoleColors.GREEN+"PERCEPTION"+ConsoleColors.RESET+": A vibrant plant sits in a polished ceramic pot, though some soil has been removed, exposing the rough, cracked surface beneath. \n" +
-                "The leaves are still lush and green, but you can see the roots peeking through the disturbed soil.");
+        if (!isUsed()) {
+            System.out.println(ConsoleColors.RED+"ACTION"+ConsoleColors.RESET+": You move the potted plant and even remove some of the dirt, but nothing of interest here.");
+            setDescription("a plant in a pot");
+            setInspection(ConsoleColors.GREEN+"PERCEPTION"+ConsoleColors.RESET+": A vibrant plant sits in a polished ceramic pot, though some soil has been removed, exposing the rough, cracked surface beneath. \n" +
+                    "The leaves are still lush and green, but you can see the roots peeking through the disturbed soil.");
+            setUsed(true);
+        } else {
+            System.out.println(ConsoleColors.RED+"ACTION"+ConsoleColors.RESET+": You've done enough damage here already. Let's poke around the room some more.");
+        }
+
     }
 }
