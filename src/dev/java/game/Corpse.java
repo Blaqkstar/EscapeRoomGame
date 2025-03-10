c80d0c649b8ef5136652c30c5b2de1d4132d1e54
Blaqkstar <111619118+Blaqkstar@users.noreply.github.com>
2025-03-09 21:08:53 -0500

    Added final two items (diary, corpse) to Conservatory

---
+++ src/dev/java/game/Corpse.java
@@ -0,0 +1,16 @@
+package game;
+
+import java.awt.*;
+import java.util.Random;
+
+public class Corpse extends Item {
+
+    public Corpse(String name, String description, String inspection) {
+        super(name, description, inspection);
+    }
+
+    @Override
+    public void use() {
+        System.out.println(ConsoleColors.RED+"ACTION"+ConsoleColors.RESET+"You're a real freak, aren't ya?");
+    }
+}
---
+++ src/dev/java/game/Diary.java
@@ -0,0 +1,16 @@
+package game;
+
+import java.awt.*;
+import java.util.Random;
+
+public class Diary extends Item {
+
+    public Diary(String name, String description, String inspection) {
+        super(name, description, inspection);
+    }
+
+    @Override
+    public void use() {
+
+    }
+}
