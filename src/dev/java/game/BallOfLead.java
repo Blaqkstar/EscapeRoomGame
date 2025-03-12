d4c62bb44eab4a699df4e745b96181faf1244ba9
kylejfavorite <kylejfavorite@gmail.com>
2025-03-12 12:22:44 -0500

    Text fixes in BallOfLead

--- src/dev/java/game/BallOfLead.java
+++ src/dev/java/game/BallOfLead.java
@@ -21,7 +21,7 @@ public class BallOfLead extends Item{
     @Override
     public void use() {
         if (transmorgrifier.isUsable() && transmorgrifier.isBeingUsed()) {
-            TransmorgSupplier<Key> keySupplier = () -> new Key("key", "a shiny new "+ConsoleColors.CYAN+"key."+ConsoleColors.RESET, ConsoleColors.GREEN+ "PERCEPTION" +ConsoleColors.RESET+": The "+ConsoleColors.CYAN+"key"+ConsoleColors.RESET + " has a faint glow as if it is brimming with energy.", room);
+            TransmorgSupplier<Key> keySupplier = () -> new Key("key", "a shiny new "+ConsoleColors.CYAN+"key"+ConsoleColors.RESET, ConsoleColors.GREEN+ "PERCEPTION" +ConsoleColors.RESET+": The "+ConsoleColors.CYAN+"key"+ConsoleColors.RESET + " has a faint glow as if it is brimming with energy.", room);
             itemToSpitToUser = keySupplier.get();
             itemToSpitToUser.setObserved(true);
             room.setItem(Main.Direction.south, itemToSpitToUser); // adds key to the north
@@ -29,10 +29,10 @@ public class BallOfLead extends Item{
             transmorgrifier.getItemsToTransmorgrify().remove(this);
             ///  TODO: Add key to items to transmorgrify. If they transmorg it, they are locked in the room forever
 
-            System.out.println(ConsoleColors.RED + "ACTION" + ConsoleColors.RESET + ": The ball of " + ConsoleColors.CYAN + "lead" + ConsoleColors.RESET + " transforms into a " + ConsoleColors.CYAN + "key." + ConsoleColors.RESET);
+            System.out.println(ConsoleColors.RED + "ACTION" + ConsoleColors.RESET + ": The ball of " + ConsoleColors.CYAN + "lead" + ConsoleColors.RESET + " transforms into a " + ConsoleColors.CYAN + "shiny new key." + ConsoleColors.RESET);
         }
         else{
-            System.out.println("You look around the room but don't see anything this would be used for.");
+            System.out.println(ConsoleColors.RED + "ACTION" + ConsoleColors.RESET + ": You look around the room but don't see anything this would be used for.");
         }
     }
 }
